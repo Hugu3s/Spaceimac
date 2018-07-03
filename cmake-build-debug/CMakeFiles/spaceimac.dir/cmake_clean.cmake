1ca4db21cc2cd539a199c78c37d3aa2c69cc7ad4
Unknown <marc.blactot@gmail.com>
2018-07-03 22:42:44 +0200

    test makefile

---
+++ cmake-build-debug/CMakeFiles/spaceimac.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/spaceimac.dir/TP_Spaceimac/main.cpp.obj"
+  "spaceimac.pdb"
+  "spaceimac.exe"
+  "spaceimac.exe.manifest"
+  "libspaceimac.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/spaceimac.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/spaceimac.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for spaceimac.
+# This may be replaced when dependencies are built.
